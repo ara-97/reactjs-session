6a7e65feaeee7785e0fd5801b45901f2228d365a
ara-97 <aravind.m@acko.com>
2020-08-08 18:41:01 +0530

    Basics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,58 @@
 import React, { Component } from 'react';
 
+import Child from './components/Child';
+
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    console.log('constructor');
+    super();
+    this.state = {
+      count: 2,
+      show: false
+    };
+  }
+
+  doIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  componentDidUpdate = () => {
+    console.log('update...');
+  };
+
+  componentDidMount = () => {
+    console.log('mounted');
+  };
+
+  toggleComp = () => {
+    this.setState({ show: !this.state.show });
+  };
+
   render() {
-    return <div>Hello</div>;
+    console.log('render');
+
+    return (
+      <div className="App">
+        {this.state.show ? <Child1 /> : null}
+        <button onClick={this.toggleComp}>Toggle</button>
+      </div>
+    );
+  }
+}
+
+class Child1 extends Component {
+  constructor(props) {
+    super();
+  }
+
+  componentWillUnmount = () => {
+    console.log('unmounting..');
+  };
+
+  render() {
+    return <div>Child</div>;
   }
 }
 
